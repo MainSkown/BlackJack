e59f1770c56db48ab4576c18a696881caf3e3228
MainSkown <mainskown.contact@gmail.com>
2025-04-22 18:02:19 +0200

    Added new font

--- app/src/main/java/com/mainskown/blackjack/ui/theme/Type.kt
+++ app/src/main/java/com/mainskown/blackjack/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.mainskown.blackjack.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mainskown.blackjack.R
+
+val Fascinate = FontFamily(
+    Font(R.font.fascinate_inline_regular, FontWeight.Normal, FontStyle.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,12 +21,11 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Fascinate,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontSize = 48.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
@@ -29,6 +35,5 @@ val Typography = Typography(
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
