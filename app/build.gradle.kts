3ceac0668d8790afd799395e09af6b2faa566ec2
MainSkown <mainskown.contact@gmail.com>
2025-05-11 17:16:07 +0200

    Intalled Compoe Colorful Sliders package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.SmartToolFactory:Compose-Colorful-Sliders:1.2.2")
 }
