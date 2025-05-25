250f73c0c4d675eb511594fe384775c4d13409a6
MainSkown <mainskown.contact@gmail.com>
2025-05-25 15:56:36 +0200

    Created Styles file

--- app/src/main/java/com/mainskown/blackjack/models/Styles.kt
+++ app/src/main/java/com/mainskown/blackjack/models/Styles.kt
@@ -1,2 +1,42 @@
 package com.mainskown.blackjack.models
 
+enum class CardStyle {
+    CLASSIC {
+        override fun toString(): String {
+            return "Classic";
+        }
+    },
+    MODERN {
+        override fun toString(): String {
+            return "Modern";
+        }
+    },
+}
+
+enum class BackgroundStyle {
+    RED {
+        override fun toString(): String {
+            return "red";
+        }
+    },
+    BLUE {
+        override fun toString(): String {
+            return "blue";
+        }
+    },
+    GREEN {
+        override fun toString(): String {
+            return "green";
+        }
+    },
+    CAT {
+        override fun toString(): String {
+            return "cat";
+        }
+    },
+    ACE_CAT {
+        override fun toString(): String {
+            return "ace_cat";
+        }
+    },
+}
