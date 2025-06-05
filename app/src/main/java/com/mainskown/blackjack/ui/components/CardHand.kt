a5c6073b119fe424bfe908746fcb0e2d04182c7c
MainSkown <mainskown.contact@gmail.com>
2025-06-05 17:43:06 +0200

    Small correction

--- app/src/main/java/com/mainskown/blackjack/ui/components/CardHand.kt
+++ app/src/main/java/com/mainskown/blackjack/ui/components/CardHand.kt
@@ -10,8 +10,8 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionOnScreen
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.mainskown.blackjack.models.Card
@@ -28,8 +28,7 @@ fun displayCardHand(
     globalPositionRead: ((Offset) -> Unit)? = null
 ) {
     val density = LocalDensity.current
-    val configuration = LocalConfiguration.current
-    val screenWidthDp = configuration.screenWidthDp.dp
+    val screenWidthDp = LocalWindowInfo.current.containerSize.width.dp
     val maxHandWidth = screenWidthDp * 0.95f // 95% of screen width
     val cardWidthPx = with(density) { cardSize.toPx() * 0.7f } // 0.7: overlap factor
     val maxCardsWidth = cardWidthPx * (cards.size - 1) + with(density) { cardSize.toPx() }
